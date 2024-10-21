1f12b5b82b4394e10abeb386dd6ef19303e99e02
zzwad37 <zawad.nafi37@gmail.com>
2024-10-21 14:28:03 -0400

    dont really remember what i changed

--- themes/demo/_layouts/default.blade.php
+++ themes/demo/_layouts/default.blade.php
@@ -5,7 +5,7 @@ description: Default layout
 <html xmlns="http://www.w3.org/1999/xhtml" lang="{{ App::getLocale() }}">
 <head>
     @partial('head')
-    <link rel="stylesheet" href="{{ asset('themes/demo/assets/font-awesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body class="d-flex flex-column h-100 {{ $this->page->bodyClass }}">
     <header id="main-header">
@@ -30,5 +30,5 @@ description: Default layout
         @partial('footer')
     </footer>
     <script type="text/javascript" src="{{ asset('app/admin/assets/js/admin.js') }}" id="app-js"></script>
-    {!! get_script_tags() !!}</body>
+    {!! get_script_tags(a) !!}</body>
 </html>
